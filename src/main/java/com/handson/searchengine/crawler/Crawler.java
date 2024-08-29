002bbdda978319302a2224e1998ae7a356b948cf
bar-schtalman <barschtalman@gmail.com>
2024-08-29 06:02:41 +0300

    with elasticsearch

--- src/main/java/com/handson/searchengine/crawler/Crawler.java
+++ src/main/java/com/handson/searchengine/crawler/Crawler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.handson.searchengine.kafka.Producer;
 import com.handson.searchengine.model.*;
+import com.handson.searchengine.util.ElasticSearch;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
@@ -32,6 +33,9 @@ public class Crawler {
 
     @Autowired
     Producer producer;
+
+    @Autowired
+    ElasticSearch elasticSearch;
     protected final Log logger = LogFactory.getLog(getClass());
 
 
@@ -48,6 +52,7 @@ public class Crawler {
         setCrawlStatus(crawlId,CrawlStatus.of(rec.getDistance(), rec.getStartTime(),0,stopReason));
         if (stopReason == null){
             Document webPageContent = Jsoup.connect(rec.getUrl()).get();
+            indexElasticSearch(rec,webPageContent);
             List<String> innerUrls = extractWebPageUrls(rec.getBaseUrl(), webPageContent);
             addUrlsToQueue(rec, innerUrls, rec.getDistance() +1);
         }
@@ -80,6 +85,12 @@ public class Crawler {
         logger.info(">> extracted->" + links.size() + " links");
 
         return links;
+    }
+    private void indexElasticSearch(CrawlerRecord rec, Document webPageContent) {
+        logger.info(">> adding elastic search for webPage: " + rec.getUrl());
+        String text = String.join(" ", webPageContent.select("a[href]").eachText());
+        UrlSearchDoc searchDoc = UrlSearchDoc.of(rec.getCrawlId(), text, rec.getUrl(), rec.getBaseUrl(), rec.getDistance());
+        elasticSearch.addData(searchDoc);
     }
     private void initCrawlInRedis(String crawlId) throws JsonProcessingException {
         setCrawlStatus(crawlId, CrawlStatus.of(0, System.currentTimeMillis(),0,  null));
